caf0b428804607f6148dfac841bfeb46735ca441
sleonia <sleonia@student.21-school.ru>
2020-08-17 13:43:36 +0300

    Fixed package_json and testing react-features

--- src/index.js
+++ src/index.js
@@ -2,7 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-ReactDOM.render(
-	<h1>Здравствуй, User</h1>, document.getElementById('root')
-);
+const name = "Michael";
+var nameUser = () => "Franklin";
+const element = <h1>Здравствуй, {name}</h1>;
+const element_2 = <h1>Здравствуй, {nameUser()}</h1>;
+
+ReactDOM.render(element, document.getElementById("root"));
+ReactDOM.render(element_2, document.getElementById("root_2"));
+
+
+//ReactDOM.render(
+	//<h1>Здравствуй, User</h1>, document.getElementById('root')
+//);
 
