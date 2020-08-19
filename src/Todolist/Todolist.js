27041ee669b4319d88699463e90fee75498f6425
sleonia <sleonia@student.21-school.ru>
2020-08-19 13:36:27 +0300

    Added handler to title

--- src/Todolist/Todolist.js
+++ src/Todolist/Todolist.js
@@ -1,13 +1,27 @@
 import React from "react";
 import TodoItem from "./TodoItem/TodoItem";
 import "./TodoList.css";
-import List from "./TodoItem/List";
-import Item from "./TodoItem/Item";
 
 class TodoList extends React.Component {
 	constructor(props) {
 		super(props);
 		this.json = JSON.parse(JSON.stringify(require("./content.json")));
+		this.state = { isCompleted: false };
+
+		this.completedStyle = {
+			fontStyle: "italic",
+			color: "#cdcdcd",
+			textDecoration: "line-through"
+		}
+	}
+
+
+	handler = () => {
+		this.setState(previousState => {
+			return {
+				isCompleted: !previousState.isCompleted
+			}
+		})
 	}
 	
 	render (props) {
@@ -15,11 +29,11 @@ class TodoList extends React.Component {
 			<div>
 				{this.json.lists.map((obj) => (
 					<div className="todo-list">
-					<p className="title">{obj.title}</p>
-					{obj.items.map((item, key) => {
-						return <TodoItem item={item} />
-					})}
-				</div>))}
+						<p style={this.state.isCompleted ? this.completedStyle : null} className="title" onClick={this.handler.bind(this)}>{obj.title}</p>	
+						{obj.items.map((item, key) => {
+							return <TodoItem item={item} />
+						})}
+					</div>))}
 			</div>);
 	}
 };
