b4f7fe82d31698c649cfa35c76a90b780a574619
sleonia <sleonia@student.21-school.ru>
2020-08-30 02:02:05 +0300

    Moved Link

---
+++ src/Components/Link/Link.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Link.module.css';
+
+const Link = ({ active, children, onClick }) => (
+  <button
+    className={styles.link}
+    type="button"
+    onClick={onClick}
+    disabled={active}
+  >
+    {children}
+  </button>
+);
+
+Link.propTypes = {
+  active: PropTypes.bool.isRequired,
+  children: PropTypes.node.isRequired,
+  onClick: PropTypes.func.isRequired,
+};
+
+export default Link;
